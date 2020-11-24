90aea650a9269b50a07a727380b487fb223246d7
benl95 <beno_1995@hotmail.com>
2020-11-24 14:56:56 +0100

    Render BarChart component to app

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import * as d3 from 'd3';
+import Header from './Header';
+import BarChart from './BarChart';
+import Data from '../store/fuelData';
 
-class App extends React.Component {}
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Header title="Hoe duurzaam rijdt de Nederlandse bevolking?" />
+				<BarChart data={Data} width={960} height={500} />
+			</div>
+		);
+	}
+}
 
 export default App;
