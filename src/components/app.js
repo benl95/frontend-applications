d01dd96940068089c51bae197b9c03713e3566d3
benl95 <beno_1995@hotmail.com>
2020-11-25 15:43:47 +0100

    Remove data prop

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import BarChart from './BarChart';
-import Data from '../store/fuelData';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
 				<Header title="Hoe duurzaam rijdt de Nederlandse bevolking?" />
-				<BarChart data={Data} width={960} height={500} />
+				<BarChart width={960} height={500} />
 			</div>
 		);
 	}
