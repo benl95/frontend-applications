6a566b694c7c9bcd395b30541e203aaeb88dde21
benl95 <beno_1995@hotmail.com>
2020-11-25 15:45:04 +0100

    Split fuelData into seperate files

---
+++ src/store/mainFuelData.js
@@ -0,0 +1,7 @@
+const mainFuelData = [
+	{ fuel: 'Benzine', amount: 71291 },
+	{ fuel: 'Diesel', amount: 20941 },
+	{ fuel: 'Elektriciteit', amount: 6497 },
+];
+
+export default mainFuelData;
---
+++ src/store/minorFuelData.js
@@ -0,0 +1,8 @@
+const minorFuelData = [
+	{ fuel: 'Alcohol', amount: 57 },
+	{ fuel: 'CNG', amount: 177 },
+	{ fuel: 'LNG', amount: 38 },
+	{ fuel: 'Waterstof', amount: 8 },
+];
+
+export default minorFuelData;
