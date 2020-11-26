0d7aa08c1008493a2475b0cc610702a0842da225
benl95 <beno_1995@hotmail.com>
2020-11-26 22:19:42 +0100

    Add styles for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 class Header extends React.Component {
 	render() {
+		const styleConfig = {
+			fontSize: '30px',
+			textAlign: 'center',
+		};
 		return (
 			<div>
-				<h1>{this.props.title}</h1>
+				<h1 style={styleConfig}>{this.props.title}</h1>
 			</div>
 		);
 	}
