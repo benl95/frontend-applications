8eb7410284c46fc7f5de5e79cd38e73bc42dc981
benl95 <beno_1995@hotmail.com>
2020-11-26 17:04:48 +0100

    Create emissionCode dataset

---
+++ src/store/emissionCode.js
@@ -0,0 +1,12 @@
+const emissionCode = [
+	{ group: '0', amount: 1159 },
+	{ group: '1', amount: 152 },
+	{ group: '2', amount: 477 },
+	{ group: '3', amount: 1304 },
+	{ group: '4', amount: 8492 },
+	{ group: '5', amount: 14488 },
+	{ group: '6', amount: 27897 },
+	{ group: 'Unknown', amount: 46031 },
+];
+
+export default emissionCode;
