011f3d6b151337d6d335872eeb8a3494d24afe40
benl95 <beno_1995@hotmail.com>
2020-11-26 17:04:39 +0100

    Create co2Emission dataset

---
+++ src/store/co2Emission.js
@@ -0,0 +1,9 @@
+const co2Emission = [
+	{ group: '37-161', amount: 37531 },
+	{ group: '162-261', amount: 13241 },
+	{ group: '262-362', amount: 1595 },
+	{ group: '363-999', amount: 143 },
+	{ group: 'Unknown', amount: 47492 },
+];
+
+export default co2Emission;
